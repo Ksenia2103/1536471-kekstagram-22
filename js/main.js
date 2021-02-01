b81403a815df1e5f67e4455ae34e47153a987cf2
Kseniy <kseniy2103@yandex.ru>
2021-02-01 23:32:13 +0700

    Добавление функций

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomNumber = function (min, max) {
   if (min < max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
@@ -8,7 +7,6 @@ const getRandomNumber = function (min, max) {
 
 alert(getRandomNumber(1, 10));
 
-//Функция для проверки максимальной длины строки
 const checkStringLength = function (string, maxLength) {
   return string.length <= maxLength;
 }
