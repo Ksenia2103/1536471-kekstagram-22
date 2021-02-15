77cb91c9305c4ea69f8c34f5727bcd8262f718af
Kseniy <kseniy2103@yandex.ru>
2021-02-15 18:56:17 +0700

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,16 @@
+const getRandomNumber = function (min, max) {
+  if (min < max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  }
+  alert('Минимальное значение не может быть больше максимального!');
+}
+
+const checkStringLength = function (string, maxLength) {
+  return string.length <= maxLength;
+}
+
+function getRandomArrayElement(array) {
+  return array[getRandomNumber(0, array.length - 1)];
+}
+
+export {getRandomNumber, checkStringLength, getRandomArrayElement};
